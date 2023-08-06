489a67c910e8c3cc0eebff185355068e2c94abd5
Stephanie Wetzel <st.wetzel96@hotmail.com>
2023-08-06 13:54:22 +0200

    added JSON Array

---
+++ script.js
@@ -0,0 +1,69 @@
+let questions = [
+    {
+        "question": "Was bedeutet FPS?",
+        "answer_1": "free pocket space",
+        "answer_2": "for permanent static",
+        "answer_3": "frames per second",
+        "answer_4": "final phasing stage",
+        "right_answer": 3
+    },
+    {
+        "question": "Was ist ein Bot?",
+        "answer_1": "Spülmaschinentab",
+        "answer_2": "Roboter",
+        "answer_3": "Rakete",
+        "answer_4": "Waschmittel",
+        "right_answer": 2
+    },
+    {
+        "question": "Wofür steht die Abkürzung afk?",
+        "answer_1": "away from kitchen",
+        "answer_2": "away from keyboard",
+        "answer_3": "away from kid",
+        "answer_4": "away from kill",
+        "right_answer": 2
+    },
+    {
+        "question": "Was ist ein Bug?",
+        "answer_1": "Insekt",
+        "answer_2": "Auto",
+        "answer_3": "Schiffsvorderteil",
+        "answer_4": "Fehler",
+        "right_answer": 4
+    },
+    {
+        "question": "Die Abkürzung GG steht für...?",
+        "answer_1": "Good Game",
+        "answer_2": "Good God",
+        "answer_3": "Good Gear",
+        "answer_4": "Good Grace",
+        "right_answer": 1
+    },
+    {
+        "question": "Was ist eine Hitbox?",
+        "answer_1": "Geschenkebox",
+        "answer_2": "Musikspieler",
+        "answer_3": "Kollisionsbereich",
+        "answer_4": "Boxring",
+        "right_answer": 3
+    },
+    {
+        "question": "Was macht ein Camper?",
+        "answer_1": "Die Natur genießen.",
+        "answer_2": "Ein Zelt aufschlagen.",
+        "answer_3": "Abwarten und Tee trinken.",
+        "answer_4": "Lagerfeuerlieder singen.",
+        "right_answer": 3
+    },
+    {
+        // Hier noch eine Antwort ausdenken
+        "question": "Ein Easter Egg bedeutet...?",
+        "answer_1": "Frohe Ostern!",
+        "answer_2": "Mahlzeit!",
+        "answer_3": "",
+        "answer_4": "Überraschung!",
+        "right_answer": 4
+    }
+];
+
+
